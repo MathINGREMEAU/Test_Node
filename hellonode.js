0eff9bdec9ba9ba6a5cf8471adfd5145e50a623f
Mathieu.INGREMEAU <mathieu.ingremeau@gmail.com>
2024-10-22 14:55:51 +0200

    recommencer depot

--- hellonode.js
+++ hellonode.js
@@ -3,20 +3,12 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-
-
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
     //Set the response HTTP header with HTTP status and Content type
     res.statusCode = 200;
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    function addSum(a, b) {
-        c = a + b;
-        return c
-    }
-    addSum(5, 6);
-    res.write(JSON.stringify(c));
-    res.end()
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World\n");
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
